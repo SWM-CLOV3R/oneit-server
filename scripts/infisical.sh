b56029ca41254dac21f4f8b44c90d7d38fdde19a
Sungyeon <csy357@gmail.com>
2024-08-01 03:13:38 +0900

    [ci] infisical 환경변수 파일 저장 경로 수정

--- scripts/infisical.sh
+++ scripts/infisical.sh
@@ -15,10 +15,10 @@ export ACTIVE_ENV=dev
 export INFISICAL_TOKEN=$(infisical login --method=universal-auth --client-id=$CLIENT_ID --client-secret=$CLIENT_SECRET --silent --plain)
 
 # 4. INFISICAL 환경변수 파일 가져오기
-infisical secrets --projectId=$PROJECT_ID --env=$ACTIVE_ENV --recursive > secrets_raw.txt
+infisical secrets --projectId=$PROJECT_ID --env=$ACTIVE_ENV --recursive > /home/ubuntu/oneit/secrets_raw.txt
 
 # 5. 표 형식의 데이터를 KEY=VALUE 형식으로 변환하여 .env 파일로 저장
-awk 'BEGIN {FS="│"} NR>2 {gsub(/^ +| +$/, "", $2); gsub(/^ +| +$/, "", $3); if ($2 && $3) print $2 "=" $3}' secrets_raw.txt > /home/ubuntu/oneit/.env
+awk 'BEGIN {FS="│"} NR>2 {gsub(/^ +| +$/, "", $2); gsub(/^ +| +$/, "", $3); if ($2 && $3) print $2 "=" $3}' /home/ubuntu/oneit/secrets_raw.txt > /home/ubuntu/oneit/.env
 
 # 6. .env 파일을 읽어 환경변수 설정
 export $(cat /home/ubuntu/oneit/.env | xargs)
