2895050dcc24cdec3872cbd2753820b11bfda685
Sungyeon <csy357@gmail.com>
2024-08-01 02:06:50 +0900

    [ci] 개행 없애기

--- scripts/start.sh
+++ scripts/start.sh
@@ -5,6 +5,4 @@ JAR_NAME=$(ls $REPOSITORY/build/libs/ | grep '.jar' | tail -n 1)
 JAR_PATH=$REPOSITORY/build/libs/$JAR_NAME
 
 echo "> $JAR_PATH deploy"
-nohup java -jar \
-        -Dspring.profiles.active=dev \
-        $JAR_PATH > $REPOSITORY/nohup.out 2>&1 &
+nohup java -jar -Dspring.profiles.active=dev $JAR_PATH > $REPOSITORY/nohup.out 2>&1 &
