75fb83701e1026d6402ea9f51788625017ede01a
Sungyeon <csy357@gmail.com>
2024-08-01 03:22:37 +0900

    [ci] start.sh debugging

--- scripts/start.sh
+++ scripts/start.sh
@@ -7,4 +7,6 @@ JAR_NAME=$(ls $REPOSITORY/build/libs/ | grep '.jar' | tail -n 1)
 JAR_PATH=$REPOSITORY/build/libs/$JAR_NAME
 
 echo "> $JAR_PATH deploy"
+echo "> nohup java -jar -Dspring.profiles.active=dev $JAR_PATH > $REPOSITORY/nohup.out 2>&1 &"
+echo "> $RDS_USER : $RDS_ENDPOINT"
 nohup java -jar -Dspring.profiles.active=dev $JAR_PATH > $REPOSITORY/nohup.out 2>&1 &
