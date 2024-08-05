23859848b252d2b9a586cde79070a7888122529d
Sungyeon <csy357@gmail.com>
2024-08-05 19:16:52 +0900

    [refactor] swagger config 통합

--- src/main/java/clov3r/oneit_server/config/SwaggerConfig.java
+++ src/main/java/clov3r/oneit_server/config/SwaggerConfig.java
@@ -5,34 +5,48 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
+import java.util.Collections;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
+import org.springframework.core.env.Environment;
 
 @Configuration
-@Profile("local")
+@RequiredArgsConstructor
 public class SwaggerConfig {
+
+    @Value("${spring.request.url}")
+    String requestUrl;
+
+    private final Environment environment;
+
     private Info apiInfo() {
         return new Info()
                 .title("One!t API") // Title of the API
                 .description("One!t API") // Description of the API
                 .version("v1.0.0"); // Version of the API
     }
+
     @Bean
     public OpenAPI openAPI() {
 
         SecurityScheme apiKey = new SecurityScheme()
-                .type(SecurityScheme.Type.APIKEY)
-                .in(SecurityScheme.In.HEADER)
-                .name("Authorization");
+            .type(SecurityScheme.Type.APIKEY)
+            .in(SecurityScheme.In.HEADER)
+            .name("Authorization");
 
         SecurityRequirement securityRequirement = new SecurityRequirement()
-                .addList("Bearer Token");
+            .addList("Bearer Token");
 
-        return new OpenAPI()
-                .components(new Components().addSecuritySchemes("Bearer Token", apiKey))
-                .addSecurityItem(securityRequirement)
-                .info(apiInfo());
+        OpenAPI openAPI = new OpenAPI()
+            .components(new Components().addSecuritySchemes("Bearer Token", apiKey))
+            .addSecurityItem(securityRequirement)
+            .info(apiInfo())
+            .servers(Collections.singletonList(new Server().url(requestUrl)));
+
+        return openAPI;
     }
 
 }
