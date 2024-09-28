3ca22ac3e9b42ba0c3328e0f87b22b66a48b3f68
Sungyeon <csy357@gmail.com>
2024-09-29 01:50:21 +0900

    [chore] 예상치 못한 에러 발생시 Slack Alarm 오도록 설정 + Log pattern 수정

--- src/main/java/clov3r/oneit_server/OneitServerApplication.java
+++ src/main/java/clov3r/oneit_server/OneitServerApplication.java
@@ -1,8 +1,5 @@
 package clov3r.oneit_server;
 
-import clov3r.oneit_server.config.security.JwtTokenProvider;
-import clov3r.oneit_server.config.security.TokenProvider;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
