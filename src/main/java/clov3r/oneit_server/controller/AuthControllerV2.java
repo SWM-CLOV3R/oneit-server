a5b85637fd432f61127ab2a9a8330d8fc0a59ce9
Sungyeon <csy357@gmail.com>
2024-09-29 16:18:49 +0900

    [feature] ONEIT 자체 회원가입 API

--- src/main/java/clov3r/oneit_server/controller/AuthControllerV2.java
+++ src/main/java/clov3r/oneit_server/controller/AuthControllerV2.java
@@ -5,10 +5,13 @@ import clov3r.oneit_server.config.security.TokenProvider;
 import clov3r.oneit_server.domain.DTO.KakaoFriendDTO;
 import clov3r.oneit_server.domain.DTO.KakaoLoginDTO;
 import clov3r.oneit_server.domain.DTO.KakaoProfileDTO;
+import clov3r.oneit_server.domain.DTO.FriendDTO;
+import clov3r.oneit_server.domain.DTO.UserDTO;
 import clov3r.oneit_server.domain.data.AuthToken;
 import clov3r.oneit_server.domain.data.status.UserStatus;
 import clov3r.oneit_server.domain.entity.User;
 import clov3r.oneit_server.domain.request.KakaoAccessToken;
+import clov3r.oneit_server.domain.request.SignupRequest;
 import clov3r.oneit_server.repository.AuthRepository;
 import clov3r.oneit_server.repository.UserRepository;
 import clov3r.oneit_server.service.AuthService;
@@ -35,14 +38,14 @@ public class AuthControllerV2 {
     private final TokenProvider tokenProvider;
 
 
-    @Tag(name = "카카오 로그인 API", description = "카카오 로그인 관련 API 목록")
+    @Tag(name = "계정 API", description = "회원가입/로그인 관련 API 목록")
     @Operation(summary = "카카오 로그인", description = "유저의 카카오 액세스 토큰을 입력받아 자체 서비스에서 카카오 로그인을 진행합니다.")
     @PostMapping("/api/v2/kakao/login")
     public ResponseEntity<KakaoLoginDTO> kakaoLogin(@RequestBody KakaoAccessToken kakaoAccessToken) {
         KakaoProfileDTO kakaoProfileDTO = authService.getKaKaoUserInfo(kakaoAccessToken.getAccessToken());
         User user;
-        if (userRepository.existsUserByEmail(kakaoProfileDTO.getKakao_account().getEmail())) {
-            user = userRepository.findUserByEmail(kakaoProfileDTO.getKakao_account().getEmail());
+        if (userRepository.existsByEmail(kakaoProfileDTO.getKakao_account().getEmail())) {
+            user = userRepository.findByEmail(kakaoProfileDTO.getKakao_account().getEmail());
             if (!user.getStatus().equals(UserStatus.ACTIVE)) {
                 user.setStatus(UserStatus.ACTIVE); 
             }
@@ -58,7 +61,7 @@ public class AuthControllerV2 {
     }
 
     // 헤더에서 토큰을 가져와 검증한 뒤에 유효하다면 user idx를 꺼내서 유저의 정보를 반환하는 API
-    @Tag(name = "카카오 로그인 API", description = "카카오 로그인 관련 API 목록")
+    @Tag(name = "계정 API", description = "회원가입/로그인 관련 API 목록")
     @Operation(summary = "카카오 로그인 유저 조회", description = "헤더에 담긴 토큰을 검증하여 유저의 정보를 반환합니다.")
     @GetMapping("/api/v2/kakao/user")
     public ResponseEntity<User> getUserInfo (
@@ -69,13 +72,33 @@ public class AuthControllerV2 {
     }
 
     // 유저의 카카오톡 친구 목록을 가져오는 API
-    @Tag(name = "카카오 로그인 API", description = "카카오 로그인 관련 API 목록")
+    @Tag(name = "계정 API", description = "회원가입/로그인 관련 API 목록")
     @Operation(summary = "카카오 친구 목록 조회", description = "유저의 카카오톡 친구 목록을 조회합니다.")
     @GetMapping("/api/v2/kakao/friends")
     public ResponseEntity<KakaoFriendDTO> getFriends(
         @RequestBody KakaoAccessToken kakaoAccessToken
     ) {
         return ResponseEntity.ok(authService.getKakaoFriends(kakaoAccessToken.getAccessToken()));
+    }
+
+    @Tag(name = "계정 API", description = "회원가입/로그인 관련 API 목록")
+    @Operation(summary = "ONEIT 회원가입", description = "이름, 닉네임, 성별, 생년월일을 추가로 입력받습니다.")
+    @PostMapping("/api/v2/signup")
+    public ResponseEntity<UserDTO> signUp(
+        @RequestBody SignupRequest signupRequest,
+        @Parameter(hidden = true) @Auth Long userIdx
+        ) {
+        User user = userService.signUp(signupRequest, userIdx);
+        UserDTO newUser = UserDTO.builder()
+            .idx(user.getIdx())
+            .email(user.getEmail())
+            .name(user.getName())
+            .nickname(user.getNickname())
+            .profileImg(user.getProfileImg())
+            .gender(user.getGender())
+            .birthDate(user.getBirthDate())
+            .build();
+        return ResponseEntity.ok(newUser);
     }
 
 }
--- src/main/java/clov3r/oneit_server/service/UserService.java
+++ src/main/java/clov3r/oneit_server/service/UserService.java
@@ -1,9 +1,14 @@
 package clov3r.oneit_server.service;
 
+import static clov3r.oneit_server.error.errorcode.CustomErrorCode.USER_NOT_FOUND;
+
 import clov3r.oneit_server.domain.entity.User;
+import clov3r.oneit_server.domain.request.SignupRequest;
+import clov3r.oneit_server.error.exception.BaseExceptionV2;
 import clov3r.oneit_server.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -12,7 +17,22 @@ public class UserService {
     private final UserRepository userRepository;
 
     public User getUser(Long userIdx) {
-        return userRepository.findUser(userIdx);
+        return userRepository.findByUserIdx(userIdx);
+    }
+
+    @Transactional
+    public User signUp(SignupRequest signupRequest, Long userIdx) {
+        User user = userRepository.findByUserIdx(userIdx);
+        System.out.println("user = " + user);
+        if (user == null) {
+            throw new BaseExceptionV2(USER_NOT_FOUND);
+        }
+        user.setName(signupRequest.getName());
+        user.setNickname(signupRequest.getNickname());
+        user.setGender(signupRequest.getGender());
+        user.setBirthDate(signupRequest.getBirthDate());
+        userRepository.save(user);
+        return user;
     }
 
 }
