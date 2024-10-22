4b7e750972f87db9d62202d2602337185ba93146
Sungyeon <csy357@gmail.com>
2024-10-22 21:12:57 +0900

    [feature] 회원 탈퇴 API

--- api/src/main/java/clov3r/api/service/UserService.java
+++ api/src/main/java/clov3r/api/service/UserService.java
@@ -2,10 +2,19 @@ package clov3r.api.service;
 
 import static clov3r.api.error.errorcode.CustomErrorCode.USER_NOT_FOUND;
 
+import clov3r.api.domain.DTO.KakaoProfileDTO;
+import clov3r.api.domain.DTO.kakao.KakaoAlarmResponseDTO;
+import clov3r.api.domain.DTO.kakao.KakaoButton;
+import clov3r.api.domain.data.kakao.SIGNUP_COMPLETE;
+import clov3r.api.domain.data.status.Status;
+import clov3r.api.domain.data.status.UserStatus;
+import clov3r.api.domain.entity.Notification;
 import clov3r.api.domain.entity.User;
 import clov3r.api.domain.request.SignupRequest;
 import clov3r.api.error.exception.BaseExceptionV2;
 import clov3r.api.repository.UserRepository;
+import java.time.LocalDateTime;
+import java.util.Objects;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -15,6 +24,7 @@ import org.springframework.transaction.annotation.Transactional;
 public class UserService {
 
     private final UserRepository userRepository;
+    private final NotificationService notificationService;
 
     public User getUser(Long userIdx) {
         return userRepository.findByUserIdx(userIdx);
@@ -31,7 +41,20 @@ public class UserService {
         user.setGender(signupRequest.getGender());
         user.setBirthDate(signupRequest.getBirthDate());
         userRepository.save(user);
+
+        notificationService.sendSignupCompleteNotification(user);
         return user;
+    }
+
+    @Transactional
+    public void withdraw(Long userIdx) {
+        User user = userRepository.findByUserIdx(userIdx);
+        user.setStatus(UserStatus.INACTIVE);
+        user.setDeletedAt(LocalDateTime.now());
+    }
+
+    public User loginByKakao(KakaoProfileDTO kakaoProfileDTO) {
+        return null;
     }
 
 }
